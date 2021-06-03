6e20029ee47d2c4308e4180514e3c6aeffc4e0e6
t-bolt <isholasobuur@gmail.com>
2021-06-03 21:02:50 +0100

    learnt how to use rows and column by adding menus to the appbar

--- lib/main.dart
+++ lib/main.dart
@@ -3,26 +3,65 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: HomePage(),
+    home: SafeArea(
+      child: HomePage(),
+    ),
     theme: ThemeData(
       primarySwatch: Colors.blue
     ),
   ));
 }
 
+class MyAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60.0,
+      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+      color: Colors.blue,
+      child: Row(
+        children: <Widget> [
+          IconButton(
+              onPressed: null,
+              tooltip: 'Navigation Menu',
+              icon: Icon(Icons.menu)
+          ),
+          Expanded(
+            child: Text("Testing App"),
+          ),
+          IconButton(
+              onPressed: null,
+              icon: Icon(Icons.search),
+              tooltip: 'Search Menu',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Testing App"),
+    return Material(
+      child: Column(
+        children: <Widget> [
+          MyAppBar(),
+          Expanded(
+              child: Image.asset('assets/images/background.png',
+                fit: BoxFit.cover,
+                height: double.infinity,
+                width: double.infinity,)
+          ),
+        ],
       ),
-       body: Container(
-           child: Image.asset('assets/images/background.png',
-           fit: BoxFit.cover,
-           height: double.infinity,
-           width: double.infinity,
-           alignment: Alignment.center,)
-       )
+       // body: Container(
+       //     child: Image.asset('assets/images/background.png',
+       //     fit: BoxFit.cover,
+       //     height: double.infinity,
+       //     width: double.infinity,)
+       // )
     );
   }
 }
