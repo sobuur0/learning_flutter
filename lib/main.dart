b5e7bf4863f77fbdeaba7c8305585b6f573b60a2
t-bolt <isholasobuur@gmail.com>
2021-06-17 03:16:30 +0100

    learnt about the basics of stateful widget and applied it to a button

--- lib/main.dart
+++ lib/main.dart
@@ -2,95 +2,44 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: 'First App',
-    home: SafeArea(
-      child: HomePage(),
-    ),
-    theme: ThemeData(
-      primarySwatch: Colors.blue
-    ),
-  ));
+  runApp(testApp());
 }
 
-class MyAppBar extends StatelessWidget {
+class testApp extends StatefulWidget {
+  @override
+  _testAppState createState() => _testAppState();
+}
+
+
+
+class _testAppState extends State<testApp> {
+
+  String ttext = '';
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    ttext = 'Click on this Button bruh';
+  }
+
+  void changeText() {
+    setState(() {
+      ttext = 'Text is now changed';
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 60.0,
-      padding: const EdgeInsets.symmetric(horizontal: 10.0),
-      color: Colors.blue,
-      child: Row(
-        children: <Widget> [
-          IconButton(
-              onPressed: null,
-              tooltip: 'Navigation Menu',
-              icon: Icon(Icons.menu)
-          ),
-          Expanded(
-            child: Text("Testing App",
-            style: Theme.of(context)
-              .primaryTextTheme
-              .headline5),
-          ),
-          IconButton(
-              icon: Icon(Icons.search),
-              tooltip: 'Search Menu',
-              onPressed: null,
-          ),
-        ],
+    return MaterialApp(
+      title: 'Testing App With Stateful Widget',
+      home: Scaffold(
+        body: Center(
+          child: RaisedButton(onPressed: changeText, child: Text(ttext),),
+        ),
       ),
     );
   }
 }
 
-class FloatingButton extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        tooltip: 'Subtract',
-        onPressed: null,
-      ),
-    );
-  }
-}
-
-
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      child: Column(
-        children: <Widget> [
-          MyAppBar(),
-          Expanded(
-            flex: 3,
-              child: Image.asset('assets/images/background.png',
-                fit: BoxFit.cover,
-                height: double.infinity,
-                width: double.infinity,)
-          ),
-
-          Expanded(
-            flex: 1,
-              child: Container(
-                  child: FloatingButton()
-              )
-          )
-
-        ],
-      ),
-       // body: Container(
-       //     child: Image.asset('assets/images/background.png',
-       //     fit: BoxFit.cover,
-       //     height: double.infinity,
-       //     width: double.infinity,)
-       // )
-    );
-  }
-}
 
 
